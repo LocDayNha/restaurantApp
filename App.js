a74b96e244a89ba76018d225a37e44260677cfe4
Do Hoang Phu <126312300+PhuNeBan@users.noreply.github.com>
2024-09-17 22:08:11 +0700

    import

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import DetailsScreen from './src/screens/DetailsScreen';
-import TableSelectionScreen from './src/screens/TableSelectionScreen';
-import ChooseTableScreen from './src/screens/ChooseTableScreen';
-import SuccessScreen from './src/screens/SuccessScreen';
-import SuccessScreen_Two from './src/screens/SuccessScreen_Two';
-import BookingScreen from './src/screens/BookingScreen';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {
+  BookingScreen,
+  ChooseTableScreen,
+  DetailsScreen,
+  SuccessScreen,
+  SuccessScreen_Two,
+  TableSelectionScreen,
+} from './src/screens';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <BookingScreen/> */}
-      {/* <DetailsScreen/> */}
-      {/* <SuccessScreen/> */}
-      {/* <SuccessScreen_Two/> */}
-      {/* <TableSelectionScreen/> */}
-      {/* <ChooseTableScreen/> */}
+      {/* <BookingScreen />
+      <ChooseTableScreen />
+      <DetailsScreen />
+      <SuccessScreen />
+      <SuccessScreen_Two />
+      <TableSelectionScreen /> */}
     </SafeAreaView>
   );
 };
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,21 @@
 import HomeScreen from './home/HomeScreen';
 import LoginScreen from './auth/LoginScreen';
 import SignupScreen from './auth/SignupScreen';
+import BookingScreen from './BookingScreen';
+import ChooseTableScreen from './ChooseTableScreen';
+import DetailsScreen from './DetailsScreen';
+import SuccessScreen from './SuccessScreen';
+import SuccessScreen_Two from './SuccessScreen_Two';
+import TableSelectionScreen from './TableSelectionScreen';
 
-export {HomeScreen, LoginScreen, SignupScreen};
+export {
+  HomeScreen,
+  LoginScreen,
+  SignupScreen,
+  BookingScreen,
+  ChooseTableScreen,
+  DetailsScreen,
+  SuccessScreen,
+  SuccessScreen_Two,
+  TableSelectionScreen,
+};
