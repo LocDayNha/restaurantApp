5aa87b62a9ad06698c0abf5dcc863c8113733b9f
NDHSxKing <130552633+KingxGods1@users.noreply.github.com>
2024-09-18 18:20:02 +0700

    Tuần đầu tiên
    Xong hết giao diện và sub về tiếng Việt, Chưa code chức năng

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import WelcomeActivity1 from './src/code/WelcomeActivity1'
+import WelcomeActivity2 from './src/code/WelcomeActivity2'
+import WelcomeActivity3 from './src/code/WelcomeActivity3'
+import ProfileActivity from './src/code/ProfileActivity'
+import VerifyEmailActivity from './src/code/VerifyEmailActivity'
+import ConfirmPasswordActivity from './src/code/ConfirmPasswordActivity'
+import ForgotPasswordActivity from './src/code/ForgotPasswordActivity'
+import WelcomeActivity from './src/code/WelcomeActivity'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+      <ForgotPasswordActivity/>
   )
 }
 
---
+++ src/code/ProfileActivity.js
@@ -0,0 +1,184 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
+
+const ProfileScreen = () => {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.top}>
+          {/* Nút quay lại sử dụng ảnh thay cho icon */}
+            <TouchableOpacity style={styles.backButton}>
+              <Image source={require('../images/back.png')} style={styles.icon} /> 
+            </TouchableOpacity>
+            <Text style={styles.topdes}>Điền thông tin của bạn</Text>
+        </View>
+      
+
+      {/* Ảnh hồ sơ */}
+      <View style={styles.profileContainer}>
+        <Image
+          source={require('../images/food.jpg')}
+          style={styles.profileImage}
+        />
+        <TouchableOpacity style={styles.icon1}>
+          <Image source={require('../images/pencil.png')} style={styles.icon} /> 
+        </TouchableOpacity>
+      </View>
+
+      {/* Form thông tin người dùng */}
+      <View style={styles.form}>
+        {/* Tên đầy đủ */}
+        
+        <View style={styles.inputContainer}>
+        <Image source={require('../images/user.png')} style={styles.icon} /> 
+          <TextInput 
+          style={styles.input} 
+          placeholder="Họ và tên"
+          placeholderTextColor='#999'/>
+          </View>
+        
+        {/* Nickname
+        <View style={styles.inputContainer}>
+        <Image source={require('../images/user.png')} style={styles.icon} /> 
+          <TextInput 
+          style={styles.input} 
+          placeholder="Name"/>
+          </View>
+         */}
+        {/* Ngày sinh */}
+        <View style={styles.inputContainer}>
+        <Image source={require('../images/calendar.png')} style={styles.icon} /> 
+          <TextInput 
+            style={[styles.input, { flex: 1 }]} 
+            placeholder="Ngày, tháng, năm sinh" 
+            placeholderTextColor='#999' 
+          />
+          
+        </View>
+
+        {/* Email */}
+        <View style={styles.inputContainer}>
+        <Image source={require('../images/email.png')} style={styles.icon} /> 
+          <TextInput 
+            style={[styles.input, { flex: 1 }]} 
+            placeholder="Email" 
+            placeholderTextColor='#999' 
+          />
+          
+        </View>
+
+        {/* Số điện thoại */}
+        <View style={styles.inputContainer}>
+          <Image source={require('../images/phone-call.png')} style={styles.icon} />
+          <TextInput 
+            style={[styles.input, { flex: 1 }]} 
+            placeholder="Số điện thoại" 
+            placeholderTextColor='#999' 
+          />
+        </View>
+
+        {/* Giới tính */}
+        <View style={styles.inputContainer}>
+          <Image source={require('../images/gender.png')} style={styles.icon} /> 
+          <TextInput 
+            style={[styles.input, { flex: 1 }]} 
+            placeholder="Giới tính" 
+            placeholderTextColor='#999' />
+        </View>
+
+        </View>
+        {/* Nút Continue */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Tiếp tục</Text>
+        </TouchableOpacity>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 10,
+    zIndex: 1,
+  },
+  profileContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 80,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  icon1: {
+    width: 35,
+    height: 30,
+    top: 75,
+    right: 115,
+    position: 'absolute',
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    paddingTop: 5,
+    paddingLeft: 0,
+    borderColor:"black",
+  },
+  topdes: {
+    color:"black",
+    fontSize: 21,
+    top: 35,
+    left: 50,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginLeft: 10,
+  },
+  flagIcon: {
+    width: 30,
+    height: 20,
+    marginRight: 10,
+  },
+  form: {
+    marginTop: 40,
+  },
+  input: {
+    // color:"red",
+    backgroundColor: '#F5F5F5',
+    // paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    fontSize: 16,
+    // marginBottom: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#000',
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginTop: 45,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default ProfileScreen;
