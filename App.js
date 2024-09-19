9791f01cb70f7a6afea50ac52ce816e63eae2921
LocDayNha <vohoangloc200@gmail.com>
2024-09-19 14:25:06 +0700

    fix

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import Welcome1 from './src/main/intro/Welcome1';
-import Welcome2 from './src/main/intro/Welcome2';
-import Welcome3 from './src/main/intro/Welcome3';
-import Profile from './src/main/home/Profile';
-import VerifyEmail from './src/main/home/VerifyEmail';
-import ConfirmPassword from './src/main/home/ConfirmPassword';
-import ForgotPassword from './src/main/home/ForgotPassword';
-import Welcome from './src/main/intro/Welcome';
+import Welcome1 from './src/Main/intro/Welcome1';
+import Welcome2 from './src/Main/intro/Welcome2';
+import Welcome3 from './src/Main/intro/Welcome3';
+import Profile from './src/Main/home/Profile';
+import VerifyEmail from './src/Main/home/VerifyEmail';
+import ConfirmPassword from './src/Main/home/ConfirmPassword';
+import ForgotPassword from './src/Main/home/ForgotPassword';
+import Welcome from './src/Main/intro/Welcome';
 
 const App = () => {
   return (
