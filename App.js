5e76176e2b3fb5127c657d74487b46f7d9fce0a7
LocDayNha <vohoangloc200@gmail.com>
2024-09-19 15:49:52 +0700

    fix link

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { View, Text, SafeAreaView } from 'react-native';
 import Login from './src/main/Login/Login';
 import Login2 from './src/main/Login/Login2';
 import Register from './src/main/Login/Register';
-import Welcome1 from './src/Main/intro/Welcome1';
-import Welcome2 from './src/Main/intro/Welcome2';
-import Welcome3 from './src/Main/intro/Welcome3';
-import Profile from './src/Main/home/Profile';
-import VerifyEmail from './src/Main/home/VerifyEmail';
-import ConfirmPassword from './src/Main/home/ConfirmPassword';
-import ForgotPassword from './src/Main/home/ForgotPassword';
-import Welcome from './src/Main/intro/Welcome';
+import Welcome1 from './src/main/intro/Welcome1';
+import Welcome2 from './src/main/intro/Welcome2';
+import Welcome3 from './src/main/intro/Welcome3';
+import Profile from './src/main/home/Profile';
+import VerifyEmail from './src/main/home/VerifyEmail';
+import ConfirmPassword from './src/main/home/ConfirmPassword';
+import ForgotPassword from './src/main/home/ForgotPassword';
+import Welcome from './src/main/intro/Welcome';
 
 
 const App = () => {
