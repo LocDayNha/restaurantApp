c778cc96ec69f15864c86176c4ffe478d03a0266
LocDayNha <vohoangloc200@gmail.com>
2024-11-12 19:14:37 +0700

    Phân quyền chức năng

--- src/main/home/AppNavigator.js
+++ src/main/home/AppNavigator.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
-import React from 'react';
+import {React, useContext} from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -23,6 +23,7 @@ import OrderProcessing from '../payment/OrderProcessing';
 
 import { ConfirmPassword, ForgotPassword, Login2, Register } from '../Login';
 import DetailsScreen from '../DetailsScreen';
+import { AppContext } from '../../util/AppContext';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -42,7 +43,98 @@ const SettingScreen = () => {
     </Stack.Navigator>
   );
 };
-const Tabbar = () => {
+
+const Tabbar1 = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          height: 80,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+
+        },
+      }}>
+      <Tab.Screen
+        name="Món ăn"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#859c3e' : 'black',
+                }}
+                source={require('../../icon/menu.png')}
+              />
+              <Text
+                style={{ color: focused ? '#95AE45' : 'black', fontSize: 13 }}>
+                Món ăn
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Đặt bàn"
+        component={ChooseTableScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#859c3e' : 'black',
+                }}
+                source={require('../../icon/order_table.png')}
+              />
+              <Text
+                style={{ color: focused ? '#859c3e' : 'black', fontSize: 13 }}>
+                Đặt bàn
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Trang cá nhân"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#859c3e' : 'black',
+                }}
+                source={require('../../icon/user.png')}
+              />
+              <Text
+                style={{ color: focused ? '#859c3e' : 'black', fontSize: 13 }}>
+                Cá nhân
+              </Text>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+const Main1 = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Tabbar" component={Tabbar1} />
+    </Stack.Navigator>
+  );
+};
+
+const Tabbar2 = () => {
   return (
     <Tab.Navigator
       screenOptions={{
@@ -100,28 +192,6 @@ const Tabbar = () => {
         }}
       />
       <Tab.Screen
-        name="Đặt bàn"
-        component={ChooseTableScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: 'center' }}>
-              <Image
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#859c3e' : 'black',
-                }}
-                source={require('../../icon/order_table.png')}
-              />
-              <Text
-                style={{ color: focused ? '#859c3e' : 'black', fontSize: 13 }}>
-                Đặt bàn
-              </Text>
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
         name="Đơn hàng"
         component={Order}
         options={{
@@ -143,41 +213,21 @@ const Tabbar = () => {
           ),
         }}
       />
-      <Tab.Screen
-        name="Trang cá nhân"
-        component={SettingScreen}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: 'center' }}>
-              <Image
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#859c3e' : 'black',
-                }}
-                source={require('../../icon/user.png')}
-              />
-              <Text
-                style={{ color: focused ? '#859c3e' : 'black', fontSize: 13 }}>
-                Cá nhân
-              </Text>
-            </View>
-          ),
-        }}
-      />
     </Tab.Navigator>
   );
 };
-
-const Main = () => {
+const Main2 = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Tabbar" component={Tabbar} />
+      <Stack.Screen name="Tabbar" component={Tabbar2} />
     </Stack.Navigator>
   );
 };
 
 const Appnavigator = () => {
+
+  const { infoUser } = useContext(AppContext);
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
@@ -187,14 +237,22 @@ const Appnavigator = () => {
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="ConfirmPassword" component={ConfirmPassword} />
       <Stack.Screen name="VerifyRegister" component={VerifyRegister} />
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="BookingScreen" component={BookingScreen} />
-      <Stack.Screen name="ReservationScreen" component={ReservationScreen} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-      <Stack.Screen name="OrderDetail" component={OrderDetail} />
-      <Stack.Screen name="CheckTypePayment" component={CheckTypePayment} />
-      <Stack.Screen name="VnPayWebView" component={VnPayWebView} />
+      {infoUser.role === 1 ? (
+        <>
+          <Stack.Screen name="Main" component={Main1} />
+          <Stack.Screen name="BookingScreen" component={BookingScreen} />
+          <Stack.Screen name="ReservationScreen" component={ReservationScreen} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Main" component={Main2} />
+          <Stack.Screen name="OrderDetail" component={OrderDetail} />
+          <Stack.Screen name="CheckTypePayment" component={CheckTypePayment} />
+          <Stack.Screen name="VnPayWebView" component={VnPayWebView} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
