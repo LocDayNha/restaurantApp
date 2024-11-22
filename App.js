622ae971bae52f60ba051c494c26f94e3da914c7
LocDayNha <vohoangloc200@gmail.com>
2024-11-22 12:17:55 +0700

    Test Upload Image

--- App.js
+++ App.js
@@ -32,6 +32,7 @@ import CheckTypePayment from './src/main/payment/CheckTypePayment';
 import OrderProcessing from './src/main/payment/OrderProcessing';
 import LoginGoogle from './src/test/LoginGoogle';
 import LoginFacebook from './src/test/LoginFacebook';
+import UploadImage from './src/test/UploadImage';
 
 const App = () => {
   console.warn = () => { };
@@ -39,11 +40,14 @@ const App = () => {
   const [isShowSplash, setIsShowSplash] = useState(true);
 
   return (
-    <AppContextProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AppContextProvider>
+    // <AppContextProvider>
+    //   <NavigationContainer>
+    //     <AppNavigator />
+    //   </NavigationContainer>
+    // </AppContextProvider>
+    <SafeAreaView>
+      <UploadImage />
+    </SafeAreaView>
   );
 };
 
